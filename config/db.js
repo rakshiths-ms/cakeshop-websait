286ece017dcc46901902bf5c1ac625856414685f
rakshiths-ms <rakshithsir9@gmail.com>
2026-04-16 12:19:03 +0530

    Fix connection string syntax in db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://localhost:27017/cake-shop');
+    const conn = await mongoose.connect(mongodb://localhost:27017/cake-shop);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
